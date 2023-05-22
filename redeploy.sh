039f98fb0c0ba213b80e065c6fbf8e9f562590ea
Ray Chen <chenyuncai@tmxmall.com>
2023-05-22 15:44:22 +0800

    更新

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,5 @@
-git checkout .
-git checkout l10n_master
-git pull
 # 将文件复制到对应的目标位置
-mv i18n/connector-translated/en/blog i18n/en/docusaurus-plugin-content-blog/
-mv i18n/connector-translated/en/docs i18n/en/docusaurus-plugin-content-docs/
+mkdir -p i18n/en/docusaurus-plugin-content-docs/current/
+cp -rf i18n/connector-translated/en/blog/* i18n/en/docusaurus-plugin-content-blog/
+cp -rf i18n/connector-translated/en/docs/* i18n/en/docusaurus-plugin-content-docs/current/
 npm run build && npm run serve
