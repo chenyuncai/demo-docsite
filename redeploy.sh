743bbbd73f26a66391215def934f419ac0c759dd
Ray Chen <chenyuncai@tmxmall.com>
2023-05-22 15:14:21 +0800

    修改部署文件

--- redeploy.sh
+++ redeploy.sh
@@ -1,3 +1,7 @@
 git checkout .
+git checkout l10n_master
 git pull
+# 将文件复制到对应的目标位置
+mv i18n/connector-translated/en/blog i18n/en/docusaurus-plugin-content-blog/
+mv i18n/connector-translated/en/docs i18n/en/docusaurus-plugin-content-docs/
 npm run build && npm run serve
